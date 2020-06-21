969b42e35210845f7166e65d2c6e8dfde051cb9f
Tim Riley <tim@openmonkey.com>
2020-06-21 19:36:53 +1000

    Refer explicitly to application types

--- config/settings.rb
+++ config/settings.rb
@@ -3,15 +3,13 @@
 require "app_prototype/types"
 
 Hanami.application.settings do
-  Types = AppPrototype::Types
-
   # Framework
-  setting :log_to_stdout, Types::Params::Bool.optional.default(false)
+  setting :log_to_stdout, AppPrototype::Types::Params::Bool.optional.default(false)
 
   # Database
-  setting :database_url, Types::String
+  setting :database_url, AppPrototype::Types::String
 
   # Assets
-  setting :precompiled_assets, Types::Params::Bool.optional.default(false)
-  setting :assets_server_url, Types::String.optional.default("http://localhost:8080")
+  setting :precompiled_assets, AppPrototype::Types::Params::Bool.optional.default(false)
+  setting :assets_server_url, AppPrototype::Types::String.optional.default("http://localhost:8080")
 end
