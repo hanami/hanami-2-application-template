506e509b7769a3274d17227d0cda3ba3d7780e0e
Tim Riley <tim@openmonkey.com>
2020-08-01 20:23:33 +1000

    Add cookie session middleware
    This is required for the CSRF token to work, and is typically expected for web apps with server-rendered views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+require "rack/session/cookie"
+
 Hanami.application.routes do
+  use Rack::Session::Cookie,
+    key: "app_prototype.session",
+    secret: Hanami.application.settings.session_secret,
+    expire_after: 60 * 60 * 24 * 365 # Expire after 1 year
+
   slice :main, at: "/" do
     root to: "home.show"
   end
--- config/settings.rb
+++ config/settings.rb
@@ -9,6 +9,9 @@ Hanami.application.settings do
   # Database
   setting :database_url, AppPrototype::Types::String
 
+  # Application
+  setting :session_secret, AppPrototype::Types::String
+
   # Assets
   setting :precompiled_assets, AppPrototype::Types::Params::Bool.optional.default(false)
   setting :assets_server_url, AppPrototype::Types::String.optional.default("http://localhost:8080")
