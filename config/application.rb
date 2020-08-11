65f51083ae74a961f89a718c4bbc7f1d540e02e9
Tim Riley <tim@openmonkey.com>
2020-08-11 21:37:49 +1000

    Use config to set up cookie sessions

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,12 @@ require "hanami"
 
 module AppPrototype
   class Application < Hanami::Application
+    config.sessions = :cookie, {
+      key: "app_prototype.session",
+      secret: settings.session_secret,
+      expire_after: 60 * 60 * 24 * 365 # 1 year
+    }
+
     config.logger = {
       level: :debug,
       stream: settings.log_to_stdout ? $stdout : "log/#{Hanami.env}.log"
