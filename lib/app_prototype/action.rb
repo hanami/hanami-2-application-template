3aac52b0ec49550eade83fa492c6402ebaa72c84
Luca Guidi <me@lucaguidi.com>
2021-07-19 09:14:23 +0200

    Minimize application action code (#32)

--- lib/app_prototype/action.rb
+++ lib/app_prototype/action.rb
@@ -3,19 +3,8 @@
 
 require "json" # required for Hanami::Action::Flash to work
 require "hanami/action"
-require "hanami/action/cookies"
-require "hanami/action/csrf_protection"
-require "hanami/action/session"
 
 module AppPrototype
   class Action < Hanami::Action
-    def self.inherited(klass)
-      super
-
-      # These will need to be sorted by the framework eventually
-      klass.include Hanami::Action::Cookies
-      klass.include Hanami::Action::Session
-      klass.include Hanami::Action::CSRFProtection
-    end
   end
 end
