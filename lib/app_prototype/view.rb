f1536222880531928346558905eec710bc0d8f4e
Luca Guidi <me@lucaguidi.com>
2022-02-04 16:19:01 +0100

    Temporary delegate framework setup to `AppPrototype::View`

--- lib/app_prototype/view.rb
+++ lib/app_prototype/view.rb
@@ -4,5 +4,14 @@ require "hanami/view"
 
 module AppPrototype
   class View < Hanami::View
+    def self.inherited(klass)
+      super
+
+      klass.class_eval do
+        # This will eventually be automatic via Hanami 2 view integration
+        config.inflector = Hanami.application[:inflector]
+        config.part_namespace = Parts
+      end
+    end
   end
 end
--- slices/main/lib/view.rb
+++ slices/main/lib/view.rb
@@ -5,8 +5,5 @@ require "hanami/view"
 
 module Main
   class View < Hanami::View
-    # This will eventually be automatic via Hanami 2 view integration
-    config.inflector = Hanami.application[:inflector]
-    config.part_namespace = Parts
   end
 end
