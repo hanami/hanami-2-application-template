28186894a994a7655442efbd6e8c6ea15e767d95
Tim Riley <tim@openmonkey.com>
2022-02-06 23:16:18 +1100

    Update providers for latest Hanami/dry-system

--- config/providers/view.rb
+++ config/providers/view.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Hanami.application.register_bootable :view do |container|
+Hanami.application.register_provider :view do
   start do
     register "view.context", AppPrototype::View::Context.new
   end
