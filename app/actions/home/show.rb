19eb099184ac886ff85f3912f681a019e5347721
Tim Riley <tim@openmonkey.com>
2022-07-06 22:51:22 +0930

    Use app/ dir and latest Hanami (#88)

--- app/actions/home/show.rb
+++ app/actions/home/show.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-module Main
+module AppPrototype
   module Actions
     module Home
-      class Show < Main::Action
+      class Show < AppPrototype::Action
         def handle(*, res)
           res.body = "Welcome to AppPrototype"
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-require "hanami/application/routes"
-
 module AppPrototype
-  class Routes < Hanami::Application::Routes
+  class Routes < Hanami::Routes
     define do
-      slice :main, at: "/" do
-        root to: "home.show"
-      end
+      root to: "home.show"
     end
   end
 end
